8b507599366eaa3f97a25888414f35608d8db577
Robert Ashton <r.ashton@imperial.ac.uk>
2023-01-04 18:03:22 +0000

    Add some ctxt all cruft

--- R/reporter-buildkite.R
+++ R/reporter-buildkite.R
@@ -10,8 +10,23 @@ BuildkiteReporter <- R6::R6Class(
   "BuidlkiteReporter",
   inherit = testthat::ParallelProgressReporter,
   public = list(
+
+    ctxt_all = list(),
+
     initialize = function(...) {
       super$initialize(...)
+    },
+
+    add_result = function(context, test, result) {
+      super$add_result(context, test, result)
+      self$ctxt_all <- c(self$ctxt_all,
+                         list(list(context = context, test = test, result = result)))
+    },
+
+    show_status = function(...) {
+      super$show_status(...)
+      browser()
+      data <- super$status_data()
     }
   )
 )
