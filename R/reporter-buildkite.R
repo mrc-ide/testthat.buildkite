8c1185cb6afbd777e06d58f7c817c5c66682b09b
Rob Ashton <r.ashton@imperial.ac.uk>
2023-01-13 15:40:39 +0000

    Update to run as ParallelProgressReporter without spinner and less frequently update

--- R/reporter-buildkite.R
+++ R/reporter-buildkite.R
@@ -11,22 +11,22 @@ BuildkiteReporter <- R6::R6Class(
   inherit = testthat::ParallelProgressReporter,
   public = list(
 
-    ctxt_all = list(),
-
     initialize = function(...) {
       super$initialize(...)
+      self$update_interval <- 1
     },
 
-    add_result = function(context, test, result) {
-      super$add_result(context, test, result)
-      self$ctxt_all <- c(self$ctxt_all,
-                         list(list(context = context, test = test, result = result)))
-    },
+    update = function(force = FALSE) {
+      if (!force && !self$should_update()) return()
 
-    show_status = function(...) {
-      super$show_status(...)
-      browser()
-      data <- super$status_data()
+      message <- paste(
+        testthat:::summary_line(self$n_fail, self$n_warn, self$n_skip, self$n_ok),
+        if (length(self$files) > 0) "@" else "Starting up...",
+        paste(testthat:::context_name(names(self$files)), collapse = ", ")
+      )
+      message <- testthat:::strpad(message, self$width)
+      message <- cli::ansi_substr(message, 1, self$width)
+      self$cat_tight(self$cr(), message)
     }
   )
 )
